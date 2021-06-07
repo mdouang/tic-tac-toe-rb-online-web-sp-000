eb3fa7f7b7475434f7dd5fa3ebc222f7f44fc7bd
Maikaru <maikaru@maikaru.com>
2021-06-07 19:08:31 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,7 +18,8 @@ def move(board, index, marker)
     return true
   end
 end
-
+def position_taken?
+end
 def valid_move?(board, index)
   if !position_taken?(board, index) && (index).between?(0,8)
     return true
